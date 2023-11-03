bd92bcfc6380c3fded459dd03297d73dd78d0a6e
andersonsorato <84638520+andersonsorato@users.noreply.github.com>
2023-11-03 10:26:34 -0300

    code bt salvar

--- app/src/main/java/devandroid/asl/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/asl/applistacurso/view/MainActivity.java
@@ -3,17 +3,32 @@ package devandroid.asl.applistacurso.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import devandroid.asl.applistacurso.R;
 import devandroid.asl.applistacurso.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
     Pessoa pessoa;
+    EditText Txt_Nome;
+    Button btnSalvar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         pessoa = new Pessoa();
+
+        Txt_Nome = findViewById(R.id.txt_nome);
+        Txt_Nome.setText(pessoa.getPrimeiroNome());
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "salvar", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
