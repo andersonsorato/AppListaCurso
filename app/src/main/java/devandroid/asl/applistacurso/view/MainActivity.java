3e3a2efe85f757653cbb996137384a72ec310ac2
andersonsorato <84638520+andersonsorato@users.noreply.github.com>
2023-11-03 16:03:59 -0300

    shared preference

--- app/src/main/java/devandroid/asl/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/asl/applistacurso/view/MainActivity.java
@@ -2,6 +2,7 @@ package devandroid.asl.applistacurso.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,11 +19,14 @@ public class MainActivity extends AppCompatActivity {
     Button btnSalvar;
     Button btnLimpar;
     PessoaControl control;
+    SharedPreferences preferences;
+    public static final String NOME_PREFERENCES = "pref_lis";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        preferences = getSharedPreferences(NOME_PREFERENCES, 0);
+        SharedPreferences.Editor listaVip = preferences.edit();
         pessoa = new Pessoa();
         control = new PessoaControl();
         Txt_Nome = findViewById(R.id.txt_nome);
@@ -33,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 pessoa.setPrimeiroNome(Txt_Nome.getText().toString());
+                listaVip.putString("primeiroNome", pessoa.getPrimeiroNome());
+                listaVip.apply();
                 control.salvar(pessoa, MainActivity.this);
             }
         });
