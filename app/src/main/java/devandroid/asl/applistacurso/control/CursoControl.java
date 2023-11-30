1428a9ffd2b55b9b2c929e6d00e539f52872e1ee
andersonsorato <84638520+andersonsorato@users.noreply.github.com>
2023-11-29 22:04:38 -0300

    spinner

--- app/src/main/java/devandroid/asl/applistacurso/control/CursoControl.java
+++ app/src/main/java/devandroid/asl/applistacurso/control/CursoControl.java
@@ -1,4 +1,31 @@
 package devandroid.asl.applistacurso.control;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import devandroid.asl.applistacurso.model.curso;
+
 public class CursoControl {
+private List ListCursos;
+    public List getListaCurso(){
+        ListCursos = new ArrayList<curso>();
+        ListCursos.add(new curso("Java"));
+        ListCursos.add(new curso("PHP"));
+        ListCursos.add(new curso("Linux"));
+        ListCursos.add(new curso("Android"));
+        ListCursos.add(new curso("Pascal"));
+        ListCursos.add(new curso("Abap"));
+        ListCursos.add(new curso("Go"));
+
+        return ListCursos;
+
+    }
+    public ArrayList<String> dadosParaSpinner(){
+        ArrayList<String> dados = new ArrayList<>();
+        for (int i = 0; i < getListaCurso().size(); i++){
+            curso objeto = (curso) getListaCurso().get(i);
+           dados.add(objeto.getNomeDoCursoDesejado());
+        }
+        return dados;
+    }
 }
--- app/src/main/java/devandroid/asl/applistacurso/model/curso.java
+++ app/src/main/java/devandroid/asl/applistacurso/model/curso.java
@@ -1,4 +1,17 @@
 package devandroid.asl.applistacurso.model;
 
 public class curso {
+    private String nomeDoCursoDesejado;
+
+    public curso(String nomeDoCursoDesejado) {
+        this.nomeDoCursoDesejado = nomeDoCursoDesejado;
+    }
+
+    public String getNomeDoCursoDesejado() {
+        return nomeDoCursoDesejado;
+    }
+
+    public void setNomeDoCursoDesejado(String nomeDoCursoDesejado) {
+        this.nomeDoCursoDesejado = nomeDoCursoDesejado;
+    }
 }
