273b6fb711f716c5c3359cc23dc6993e29a54607
andersonsorato <84638520+andersonsorato@users.noreply.github.com>
2023-11-29 22:16:37 -0300

    spinner

--- app/src/main/java/devandroid/asl/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/asl/applistacurso/view/MainActivity.java
@@ -5,13 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import devandroid.asl.applistacurso.R;
+import devandroid.asl.applistacurso.control.CursoControl;
 import devandroid.asl.applistacurso.control.PessoaControl;
 import devandroid.asl.applistacurso.model.Pessoa;
+import devandroid.asl.applistacurso.model.curso;
 
 public class MainActivity extends AppCompatActivity {
     Pessoa pessoa;
@@ -20,7 +27,12 @@ public class MainActivity extends AppCompatActivity {
     Button btnLimpar;
     PessoaControl control;
     SharedPreferences preferences;
+    Spinner spinner;
+
+    List<String> nomesDosCursos;
     public static final String NOME_PREFERENCES = "pref_lis";
+    private devandroid.asl.applistacurso.control.CursoControl CursoControl;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,11 +41,23 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences.Editor listaVip = preferences.edit();
         pessoa = new Pessoa();
         control = new PessoaControl();
+        CursoControl = new CursoControl();
+        nomesDosCursos = CursoControl.dadosParaSpinner();
+        spinner = findViewById(R.id.spinner);
         pessoa.setPrimeiroNome(preferences.getString("primeiroNome",""));
         Txt_Nome = findViewById(R.id.txt_nome);
         Txt_Nome.setText(pessoa.getPrimeiroNome());
         btnSalvar = findViewById(R.id.btn_salvar);
         btnLimpar = findViewById(R.id.btn_limpar);
+
+// adapter
+        //layout
+        //injetar adapteer ao spinner
+
+        ArrayAdapter<String> adapter   = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+                CursoControl.dadosParaSpinner());
+        adapter.setDropDownViewResource(android.R.layout.simple_list_item_1);
+        spinner.setAdapter(adapter);
         btnSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
